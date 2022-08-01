c918473fd92f8ae3853de7d01f3d4ca6a8d514db
Callan Zimmermann <callanzimmermann@gmail.com>
2022-08-01 10:36:44 +0100

    4-change-loader-timeout: add delay config and necessary blocks

---
+++ Model/Config.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright © Monsoon Consulting. All rights reserved.
+ * See LICENSE_MONSOON.txt for license details
+ */
+declare(strict_types=1);
+
+namespace Monsoon\HyvaAjaxAddToCart\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Config
+ */
+class Config implements ConfigInterface
+{
+    /** @var ScopeConfigInterface */
+    private ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Is the AJAX add to cart option enabled?
+     *
+     * @param null|int|string $scopeCode
+     * @return bool
+     */
+    public function isAjaxAddToCartEnabled($scopeCode = null): bool
+    {
+        return (bool)$this->scopeConfig->isSetFlag(
+            self::XML_PATH_ENABLE_AJAX_ADD_TO_CART,
+            ScopeInterface::SCOPE_STORE,
+            $scopeCode
+        );
+    }
+
+    /**
+     * Get Ajax Add To Cart Delay
+     *
+     * @param null|int|string $scopeCode
+     * @return string
+     */
+    public function getAjaxAddToCartDelay($scopeCode = null): string
+    {
+        return (string)$this->scopeConfig->getValue(
+            self::XML_PATH_AJAX_ADD_TO_CART_DELAY,
+            ScopeInterface::SCOPE_STORE,
+            $scopeCode
+        );
+    }
+}
