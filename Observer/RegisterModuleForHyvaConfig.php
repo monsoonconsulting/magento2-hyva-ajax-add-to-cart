6b9bb334791200efcde7a8f85637f9df8a810a0a
FinnReinhardtBsc <122025487+FinnReinhardtBsc@users.noreply.github.com>
2023-11-17 11:24:45 +0100

    Create RegisterModuleForHyvaConfig.php to register module in hyva-themes.json

---
+++ Observer/RegisterModuleForHyvaConfig.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Hyvä Themes - https://hyva.io
+ * Copyright © Hyvä Themes 2022-present. All rights reserved.
+ * This category is licensed per Magento install
+ * See https://hyva.io/license
+ */
+
+declare(strict_types=1);
+
+namespace Monsoon\HyvaAjaxAddToCart\Observer;
+
+use Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class RegisterModuleForHyvaConfig implements ObserverInterface
+{
+    private $componentRegistrar;
+
+    public function __construct(ComponentRegistrar $componentRegistrar)
+    {
+        $this->componentRegistrar = $componentRegistrar;
+    }
+
+    public function execute(Observer $event)
+    {
+        $config = $event->getData('config');
+        $extensions = $config->hasData('extensions') ? $config->getData('extensions') : [];
+
+        $moduleName = implode('_', array_slice(explode('\\', __CLASS__), 0, 2));
+
+        $path = $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, $moduleName);
+
+        // Only use the path relative to the Magento base dir
+        $extensions[] = ['src' => substr($path, strlen(BP) + 1)];
+
+        $config->setData('extensions', $extensions);
+    }
+}
